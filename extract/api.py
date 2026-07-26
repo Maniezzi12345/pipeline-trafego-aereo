c56b326e016ca60f62060ed99057a826a99d530b
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-07-25 22:07:14 -0300

    feat:Extração do endpoint buscar_chegadas_voos, buscar_partidas_voos,intervalo_de_tempos_voos

--- extract/api.py
+++ extract/api.py
@@ -1,12 +1,18 @@
 import requests
 import os
 from dotenv import load_dotenv
+import pandas as pd
 load_dotenv()
 
 
 
 TOKEN_URL = "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token"
 LOCALIZACAO_URL =  "https://opensky-network.org/api/states/all?lamin=-33.7&lamax=5.2&lomin=-73.9&lomax=-34.7" 
+CHEGADAS_URL  = "https://opensky-network.org/api/flights/arrival?airport=SBGR&begin=1517227200&end=1517230800"
+PARTIDAS_URL = "https://opensky-network.org/api/flights/departure?airport=SBGR&begin=1517227200&end=1517230800"
+INTERVALO_URL = "https://opensky-network.org/api/flights/all?begin=1517227200&end=1517230800"
+
+
 def autenticacao_url(TOKEN_URL):
     response = requests.post(
         TOKEN_URL,
@@ -25,17 +31,59 @@ def autenticacao_url(TOKEN_URL):
 
 def buscar_voos_brasil(token,LOCALIZACAO_URL):
     response = requests.get(
-    LOCALIZACAO_URL,
-    headers={"Authorization": f"Bearer {token}"}
+        LOCALIZACAO_URL,
+        headers={"Authorization": f"Bearer {token}"
+
+        }
     )
     if response.status_code == 200:
         return response.json()
-    print(f"A api de buscar_voos_brasil : {requests.response}")
+    else:
+        print(f"O endpoint de buscar_voos_brasil está com erro  : {requests.response}")
+
+    # dados = response.json()
+    # df = pd.DataFrame(dados["states"])
+    # print(df)
+
+def buscar_chegadas_voos(token,CHEGADAS_URL):
+    response = requests.get(
+        CHEGADAS_URL,
+        headers={"Authorization": f"Bearer {token}"
+        }
+    )
+    if response.status_code == 200:
+            return response.json()
+    else:
+        print(f"O endpoint de buscar_chegadas_voos está com erro : {requests.response}")
 
 
+def buscar_partidas_voos(token,PARTIDAS_URL):
+    response = requests.get(
+    PARTIDAS_URL,
+    headers={"Authorization": f"Bearer {token}"
+        }
+    )
+    if response.status_code == 200:
+            return response.json()
+    else:
+            print(f"O endpoint de buscar_partidas_brasil : {requests.response}")
+
+def intervalo_de_tempos_voos(token,INTERVALO_URL):
+    response = requests.get(
+    INTERVALO_URL,
+    headers={"Authorization": f"Bearer {token}"
+        }
+    )
+    if response.status_code == 200:
+            return response.json()
+    else:
+            print(f"O endpoint de buscar_partidas_brasil : {requests.response}")
 
 resultado = autenticacao_url(TOKEN_URL)
 local = buscar_voos_brasil(resultado,LOCALIZACAO_URL)
+chegada = buscar_chegadas_voos(resultado,CHEGADAS_URL)
+partidas = buscar_partidas_voos(resultado,PARTIDAS_URL)
+intervalo = intervalo_de_tempos_voos(resultado,INTERVALO_URL)
 
-print(local)
+print(intervalo)
 
