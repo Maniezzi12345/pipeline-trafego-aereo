e018a2a383a6be003da33f270b4677b2778a1fdb
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-07-25 21:22:54 -0300

    feat:Extração do endpoint buscar_voos_brasil

--- extract/api.py
+++ extract/api.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 TOKEN_URL = "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token"
-
+LOCALIZACAO_URL =  "https://opensky-network.org/api/states/all?lamin=-33.7&lamax=5.2&lomin=-73.9&lomax=-34.7" 
 def autenticacao_url(TOKEN_URL):
     response = requests.post(
         TOKEN_URL,
@@ -17,14 +17,25 @@ def autenticacao_url(TOKEN_URL):
         }
     )
 
-
-    token = response.json()["access_token"]
-
-   
     if response.status_code == 200:
-        return token
+        return response.json()["access_token"]
     else:
-        print(f"O codigo deu erro {requests.response}")
+        print(f"A api de autenticação deu erro : {requests.response}")
+
+
+def buscar_voos_brasil(token,LOCALIZACAO_URL):
+    response = requests.get(
+    LOCALIZACAO_URL,
+    headers={"Authorization": f"Bearer {token}"}
+    )
+    if response.status_code == 200:
+        return response.json()
+    print(f"A api de buscar_voos_brasil : {requests.response}")
+
+
 
 resultado = autenticacao_url(TOKEN_URL)
-print(resultado)
+local = buscar_voos_brasil(resultado,LOCALIZACAO_URL)
+
+print(local)
+
