62838fe5eab951b9820015a97bbe69549885f90a
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-08-12 01:00:51 -0300

    fix:correção 401 persistente

--- extract/api.py
+++ extract/api.py
@@ -2,7 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import pandas as pd
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 
 
--- load/carregar.py
+++ load/carregar.py
@@ -4,10 +4,22 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
-usuario = os.getenv("DB_USER")
-senha = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-porta = os.getenv("DB_PORT")
-banco = os.getenv("DB_NAME")
 
-engine = create_engine(f"postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}")
+def criar_engine():
+    usuario = os.getenv("DB_USER")
+    senha = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    porta = os.getenv("DB_PORT")
+    banco = os.getenv("DB_NAME")
+    engine = create_engine(f"postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}")
+    return engine
+
+
+def salvar_dataframe(df,tabela,engine):
+    df.to_sql(
+        name=tabela,
+        con=engine,
+        if_exists="append",
+        index=False
+    )
+    print(f"{len(df)} registros inseridos em {tabela}")
