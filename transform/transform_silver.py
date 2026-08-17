9db65675f54b78b1e07f1c18536b76bed155b8a0
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-08-17 20:11:42 -0300

    .\.env

--- transform/transform_silver.py
+++ transform/transform_silver.py
@@ -6,8 +6,23 @@ from load.carregar import criar_engine
 
 engine = criar_engine()
 
-# # df_voos_silver = pd.read_sql("SELECT * FROM raw_voos", engine)
-# df_partidas_silver = pd.read_sql("SELECT * FROM raw_partidas",engine)
-# df_chegadas_silver = pd.read_sql("SELECT * FROM raw_chegadas",engine)
+df_voos_silver = pd.read_sql("SELECT * FROM raw_voos", engine)
+df_partidas_silver = pd.read_sql("SELECT * FROM raw_partidas",engine)
+df_chegadas_silver = pd.read_sql("SELECT * FROM raw_chegadas",engine)
 df_intervalos_silver = pd.read_sql("SELECT * FROM raw_intervalo",engine)
-print(df_intervalos_silver)
+
+frames = [df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalos_silver]
+
+def icao_maiusculo(df):
+     df = df.copy()
+     df['icao24'] = df['icao24'].str.upper()
+     return df
+
+frames = [icao_maiusculo(df) for df in frames]
+df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalo_silver = frames
+
+
+print(df_voos_silver)
+
+
+# Proximos ataques , vamos de timestamp e distancia. Construir esses frames 
