943fdff6f51bb468686567d107aa91ce50be6567
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-08-18 07:39:22 -0300

    feat: Função normalizacao_timestamp

--- transform/transform_silver.py
+++ transform/transform_silver.py
@@ -34,6 +34,9 @@ def normalizacao_timestamp(df):
      if "time_position" in df.columns and "last_contact" in df.columns:
         df['time_position'] = pd.to_datetime(df["time_position"],unit='s')
         df['last_contact'] = pd.to_datetime(df["last_contact"],unit='s')
+
+     if "lastSeen" in df.columns:
+         df['lastSeen'] = pd.to_datetime(df["lastSeen"] , unit='s')
      
 
      return df
@@ -41,4 +44,4 @@ def normalizacao_timestamp(df):
 frames = [normalizacao_timestamp(df) for df in frames]
 df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalo_silver = frames
 
-print(df_voos_silver['last_contact'])
+print(df_chegadas_silver['lastSeen'])
