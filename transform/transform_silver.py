a6bbc3998f2514927561679abe166ca66f2ec1ce
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-08-18 07:34:59 -0300

    feat: Função normalizacao_timestamp

--- transform/transform_silver.py
+++ transform/transform_silver.py
@@ -11,7 +11,10 @@ df_partidas_silver = pd.read_sql("SELECT * FROM raw_partidas",engine)
 df_chegadas_silver = pd.read_sql("SELECT * FROM raw_chegadas",engine)
 df_intervalos_silver = pd.read_sql("SELECT * FROM raw_intervalo",engine)
 
+print(df_voos_silver)
+
 frames = [df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalos_silver]
+
 
 def icao_maiusculo(df):
      df = df.copy()
@@ -19,10 +22,23 @@ def icao_maiusculo(df):
      return df
 
 frames = [icao_maiusculo(df) for df in frames]
-df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalo_silver = frames
-
-
-print(df_voos_silver)
 
 
 # Proximos ataques , vamos de timestamp e distancia. Construir esses frames 
+
+def normalizacao_timestamp(df):
+     df = df.copy()
+     if "firstSeen" in df.columns:
+        df['firstSeen'] = pd.to_datetime(df["firstSeen"], unit='s')
+
+     if "time_position" in df.columns and "last_contact" in df.columns:
+        df['time_position'] = pd.to_datetime(df["time_position"],unit='s')
+        df['last_contact'] = pd.to_datetime(df["last_contact"],unit='s')
+     
+
+     return df
+
+frames = [normalizacao_timestamp(df) for df in frames]
+df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalo_silver = frames
+
+print(df_voos_silver['last_contact'])
