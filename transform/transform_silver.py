dcec885e8c9bedc9e9cb8788bdf4e40e697c1266
João Pedro Maniezzi <joaopedromaniezzi32@gmail.com>
2026-08-19 19:48:31 -0300

    fix: Query ajuste

--- transform/transform_silver.py
+++ transform/transform_silver.py
@@ -11,57 +11,72 @@ df_partidas_silver = pd.read_sql("SELECT * FROM raw_partidas",engine)
 df_chegadas_silver = pd.read_sql("SELECT * FROM raw_chegadas",engine)
 df_intervalos_silver = pd.read_sql("SELECT * FROM raw_intervalo",engine)
 
-print(df_voos_silver['velocity'])
-
-frames = [df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalos_silver]
 
 
-def icao_maiusculo(df):
-     df = df.copy()
-     df['icao24'] = df['icao24'].str.upper()
-     return df
+df_query = pd.read_sql("""SELECT COUNT(RI.icao24) AS Quantidade,
+                                 RP.firstSeen 
+                        FROM raw_voos AS RW 
+                        JOIN raw_intevalo AS RP 
+                        ON RW.icao24 = RP.icao24 
+                        GROUP BY RP.firstSeen  
+                        HAVING Quantidade > 30  ORDER BY Quantidade DESC """,engine)
+
+print(df_query)
 
 
-def normalizacao_timestamp(df):
-     df = df.copy()
-     if "firstSeen" in df.columns:
-        df['firstSeen'] = pd.to_datetime(df["firstSeen"], unit='s')
 
-     if "time_position" in df.columns and "last_contact" in df.columns:
-        df['time_position'] = pd.to_datetime(df["time_position"],unit='s')
-        df['last_contact'] = pd.to_datetime(df["last_contact"],unit='s')
+# print(df_voos_silver['velocity'])
 
-     if "lastSeen" in df.columns:
-         df['lastSeen'] = pd.to_datetime(df["lastSeen"] , unit='s')
+
+# frames = [df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalos_silver]
+
+
+# def icao_maiusculo(df):
+#      df = df.copy()
+#      df['icao24'] = df['icao24'].str.upper()
+#      return df
+
+
+# def normalizacao_timestamp(df):
+#      df = df.copy()
+#      if "firstSeen" in df.columns:
+#         df['firstSeen'] = pd.to_datetime(df["firstSeen"], unit='s')
+
+#      if "time_position" in df.columns and "last_contact" in df.columns:
+#         df['time_position'] = pd.to_datetime(df["time_position"],unit='s')
+#         df['last_contact'] = pd.to_datetime(df["last_contact"],unit='s')
+
+#      if "lastSeen" in df.columns:
+#          df['lastSeen'] = pd.to_datetime(df["lastSeen"] , unit='s')
      
 
-     return df
+#      return df
 
 
-def normalizacao_distancia(df):
-    df = df.copy()
-    colunas = [
-        'estDepartureAirportHorizDistance',
-        'estDepartureAirportVertDistance',
-        'estArrivalAirportHorizDistance',
-        'estArrivalAirportVertDistance'
-    ]
+# def normalizacao_distancia(df):
+#     df = df.copy()
+#     colunas = [
+#         'estDepartureAirportHorizDistance',
+#         'estDepartureAirportVertDistance',
+#         'estArrivalAirportHorizDistance',
+#         'estArrivalAirportVertDistance'
+#     ]
 
-    for coluna in colunas:
-        if coluna in df.columns:
-            df[coluna] = df[coluna] / 100
-    return df
+#     for coluna in colunas:
+#         if coluna in df.columns:
+#             df[coluna] = df[coluna] / 100
+#     return df
 
-def normalizacao_velocidade(df):
-    df = df.copy()
-    df['velocity'] = df["velocity"] * 3.6
-    return df
+# def normalizacao_velocidade(df):
+#     df = df.copy()
+#     df['velocity'] = df["velocity"] * 3.6
+#     return df
 
-frame = normalizacao_velocidade(df_voos_silver)
-frames = [normalizacao_distancia(df) for df in frames]
-frames = [normalizacao_timestamp(df) for df in frames]
-frames = [icao_maiusculo(df) for df in frames]
-df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalo_silver = frames
+# frame = normalizacao_velocidade(df_voos_silver)
+# frames = [normalizacao_distancia(df) for df in frames]
+# frames = [normalizacao_timestamp(df) for df in frames]
+# frames = [icao_maiusculo(df) for df in frames]
+# df_voos_silver, df_chegadas_silver, df_partidas_silver, df_intervalo_silver = frames
 
 
-print(df_voos_silver['velocity'])
+# print(df_voos_silver['velocity'])
